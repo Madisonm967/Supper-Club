0d3bb8e848aa0b17a489baf1b09a55095c6ac25a
Madisonm967 <madisonm967@gmail.com>
2025-01-30 16:02:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,5 @@ def list_restaurants():
     return jsonify({"restaurants": restaurants}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
